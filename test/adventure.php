e662982b281f02f39a58f2e8825131b9766e3449
Lakpa Sherpa <87270447+Lakpasherpa9@users.noreply.github.com>
2022-01-11 23:10:34 +0545

    Add files via upload

--- test/adventure.php
+++ test/adventure.php
@@ -1,51 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nature Tours | Home</title>
-    <link rel="stylesheet" href="./style/adventurestyle.css">
-    <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.4/dist/css/ionicons.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.google.com/css?family=Aldrich&display=swap">
-</head>
-<body>
-    <header> <div class="container">
-        <nav>
-            <div class="nav-brand">
-                <a href="index.html">
-                    <img src="logo.png" alt="">
-                </a>
-            </div>
-           <ul class="nav-list">
-            <li class="nav-item">
-                <a href="#" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-                <a href="test.html" class="nav-link">Destination</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Pricing</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Booking</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">About</a>
-            </li>
-           </ul>
-        </nav>
-    </div>
-</header>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Adventure</title>
+        <link rel="stylesheet" href="./style/adventurestyle.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Caramel&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    
+    <body> 
+
+        <!--header-->
+
+        <nav class="navbar navbar-expand-sm navbar-dark shadow-5-strong">
+            <a href="test002.html" class="navbar-brand">
+                <img src="images/Logo 03.png" height="60px">   
+            </a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button> 
+            <div class="collapse navbar-collapse" id="navbarMenu">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#about-sec" class="nav-link">ABOUT</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a href="#contact-sec" class="nav-link">CONTACT US </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#covid-sec" class="nav-link">COVID-19 </a>
+                    </li>
+                </ul>
+            </div> 
+            <a class="cta" href="php/login.php"><button class="button1">Log In</button></a>  
+        </nav> 
+        
 <main>
     <section class="hero">
         <div class="container">
             <div class="main-message">
                 <h3>The Great Outdoors</h3>
                 <h1>Adventure</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci minima mollitia molestiae, consectetur laudantium quia optio iure esse inventore commodi modi qui sequi nulla magnam praesentium repellat voluptas possimus ipsam.</p>
-                <div class="cta">
-                    <a href="#" class="btn">Book Now</a>
                 </div>
             </div>
         </div>
@@ -55,66 +58,140 @@
             <div class="title-heading">
                 <h3>EXPERIENCE</h3>
                 <h1>THE THRILLING OUTDOORS</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
             </div>
             <div class="activities-grid">
 
-                <div class="activities-grid-item star-gazing">
+             <div class="activities-grid-item star-gazing">
             <i class="icon ion-md-star"></i>
-            <h1>Star Gazing</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet modi molestiae consectetur corporis officiis suscipit vel velit sunt minus, reiciendis recusandae ratione eum exercitationem officia voluptatum in error natus unde!</p>
+            <h1>Paragliding</h1>
+                <p>Paragliding is the recreational and competitive adventure sport of flying paragliders: lightweight, free-flying with no rigid primary structure.</p>
                 </div>
             <div class="activities-grid-item hiking">
                     <i class="icon ion-md-compass"></i>
                     <h1>Hiking</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet modi molestiae consectetur corporis officiis suscipit vel velit sunt minus, reiciendis recusandae ratione eum exercitationem officia voluptatum in error natus unde!</p>
+                    <p>Hiking is a long, vigorous walk, usually on trails or footpaths in the countryside. Walking for pleasure developed in Europe during the eighteenth century.</p>
             </div>
             <div class="activities-grid-item camping">
                             <i class="icon ion-md-bonfire"></i>
                             <h1>Camping</h1>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet modi molestiae consectetur corporis officiis suscipit vel velit sunt minus, reiciendis recusandae ratione eum exercitationem officia voluptatum in error natus unde!</p>
+                            <p>Camping is an outdoor activity involving overnight stays away from home with or without a shelter, such as a tent or a recreational vehicle.</p>
+            </div>
+            <div class="activities-grid-item rafting">
+                            <i class="icon ion-md-bonfire"></i>
+                            <h1>Rafting</h1>
+                            <p>Rafting and whitewater rafting are recreational outdoor activities which use an inflatable raft to navigate a river or other body of water</p>
             </div>
             </div>
         </div>
     </section>
-    <section class="testimonials">
-        <div class="container">
-<div class="testimonial">
-    <div class="testimonial-text-box">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem harum voluptatum fugiat illo. Omnis, sed?</p>
-        <i class="icon ion-md-quote">
-            <div class="testimonial-customer">
-                <img src="profile-pic.jpg" alt="">
-                <h1>Anonymous</h1>
-            </div>
-        </i>
-    </div>
-</div>
-        </div>
-    </section>
+    
     <section class="begin-adventure">
         <div class="container">
             <div class="title-heading">
                 <h3>Let the</h3>
                 <h1>Adventure Begin</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p>Gear up and start going to places </p>
             </div>
-            <div class="adventure-grid">
-                <div class="adventure-grid-item">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis veritatis quod ex laborum, ipsum doloremque facilis, vel nemo, quos debitis amet nesciunt enim similique sit aperiam eveniet porro eius totam!</p>
+    </section>
+    <!-- <img src="./images/pic01.jpg" width="85%" height="1%" style="margin-left: 5%"> -->
+    <!-- Covid Section -->
+    <hr>
+    <section id="covid-sec">
+            <div class="container Heading mt-4 mb-4" style="padding-top: 20px;">
+                <h2>Covid Regulations</h2>
+                <ul>
+                   <li>COVID-19 vaccination certificate showing full does of vaccination with the last does taken at least 14 days prior to arriving in Nepal,</li>
+                   <li>All foreigners arriving in Nepal must make COVID-19 test at the immigration point in Nepal. If one is found COVID-19 positive, s/he shall go to any isolation center or hospital designated by the Ministry of Health and Population.</li>
+                   <li>Travelers without certificate of full doze vaccination are not eligible for visa-one-arrival. They must take visa from the Embassy to enter into Nepal.  Such persons require to go for 10 days of hotel quarantine in Kathmandu to the designated hotels</li>
+                   <li>COVID-19 test report (RT-PCR, Gene Expert, True NAAT or WHO accredited test) showing negative result done within last 72 hours of boarding from the first airport or, if the traveler is entering through land boarder, taken within 72 hours of entry. This report will not required for the children below 5 years. </li>
+                   <li>If any symptom of COVID-19 is found or suspected you must make COVID-19 test as soon as possible and remain in isolation if found positive. 
+                       You must bear all the expenses of COVID-19 tests, hotel quarantine, hospitalization, remaining in isolation etc.</li> 
+                    <li>The list of hotels to stay in quarantine can be found <a href="https://mofa.gov.np/wp-content/uploads/2021/06/Updated-hotel-quarantine-list-by-DOT-8june-1.pdf" target="_blank"><b>here</b></a>.</li>
+
+               </ul>
+            </div>
+        </section>
+        <hr>
+        <!-- <img src="./images/pic01.jpg" width="85%" height="1%" style="margin-left: 5%"> -->
+</main>
+ <!-- footer -->
+    
+ <footer class="footer-area">
+        <div class="footer-wave-box">
+            <div class="footer-wave footer-animation"></div>
+        </div>
+        <div class="main">
+            <div class="footer">
+                <div class="single-footer">
+                    <h4>Connect With us</h4>
+                    <p>We are here to help and answer any question you might have. We look forward to hearing from you
+                    </p>
+                    <div class="footer-social">
+                        <a href="" class="color"><i class="icon fa fa-facebook"></i></a>
+                        <a href="" class="color"><i class="icon fa fa-instagram"></i></a>
+                        <a href="" class="color"><i class="icon fa fa-linkedin"></i></a>
+                        <a href="" class="color"><i class="icon fa fa-twitter"></i></a>
+
+                    </div>
+                </div>
+                <div class="single-footer">
+                    <h4>main menu</h4>
+                    <ul>
+                        <li><a href="#home-sec"><i class="fas fa-angle-right"></i> Home</a></li>
+                        <li><a href="#about-sec"><i class="fas fa-angle-right"></i> About us</a></li>
+                        <li><a href=""><i class="fas fa-angle-right"></i> Contact us</a></li>
+                    </ul>
+                </div>
+                <div class="single-footer">
+                    <h4>Helpful links</h4>
+                    <ul>
+
+                        <li><a href=""><i class="fas fa-angle-right"></i> Supports</a></li>
+                        <li><a href=""><i class="fas fa-angle-right"></i> Privacy Policy</a></li>
+                        <li><a href=""><i class="fas fa-angle-right"></i> Terms & Conditions</a></li>
+                        
+                    </ul>
+                </div>
+                <div class="single-footer">
+                    <h4>contact us</h4>
+                    
+         <section id="contact-sec">
+         <div class="contact-container">
+        <form action="./php/message.php" class="contact-form" id="contact" method="POST">
+          <div class="contact-input-container">
+            <input type="text" class="input" placeholder="Name" name="name"/>
+          </div>
+
+          <div class="contact-input-container">
+            <input type="email" class="input" placeholder="Email" name="email"/>
+          </div>
+
+          <div class="contact-input-container">
+            <input type="text" class="input" placeholder="Address" name="address" />
+          </div>
+
+          <div class="contact-input-container">
+            <textarea
+              class="msg-input"
+              rows="4"
+              placeholder="Your message"
+                name="message"
+            ></textarea>
+          </div>
+          <div class="btn-box">
+          <button type="submit" name="send" form="contact">Send</button>
+          </div>
+        </form>
+      </div>
+      </section>
+                    
+                </div>
+                <div class="single-footer">
+                <p class="copyright"> Made by <br><b> Sagyan Sachin Lakpa </b> </p>
+
 
                 </div>
-                <div class="adventure-grid-item">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis veritatis quod ex laborum, ipsum doloremque facilis, vel nemo, quos debitis amet nesciunt enim similique sit aperiam eveniet porro eius totam!</p>
-                    
-                </div>    
-            </div>
-    <a class="btn" href="#">Book Your Adventure</a>        
-        </div>
-    </section>
-</main>
-<footer>
-    
-</footer>
+            
+    </footer>
 </body>
 </html>
